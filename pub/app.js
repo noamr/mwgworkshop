8f7aa42cfd99f85c3debef8997969c53a5f52c56
Noam Rosenthal <noamr@wix.com>
2016-11-22 16:53:00 +0200

    Separate fast from naive

--- pub/app.js
+++ pub/app.js
@@ -4,6 +4,10 @@
         return fetch('/gallery-data.json').then(res => res.json());
     }
 
+    const isNaive = location.search === '?naive';
+
+    const buildGallery = isNaive ? buildGalleryNaive : buildGalleryFast;
+
     function app()
     {
         loadData()
--- pub/gallery-naive.jsx
+++ pub/gallery-naive.jsx
@@ -1,4 +1,4 @@
-function buildGallery(images)
+function buildGalleryNaive(images)
 {
     return <div id="gallery" className="naive">
         {_.map(images, image => <img src={image.low_resolution.url}/>)}
